df1a946ec8f7d900042b0adb613ab48cb0868b2f
Housein Abo Shaar <76689341+GogoIsProgramming@users.noreply.github.com>
2025-09-08 16:20:09 +0200

    chore: Fix contentful

--- examples/contentful-cms-integration/src/vendure-config.ts
+++ examples/contentful-cms-integration/src/vendure-config.ts
@@ -1,6 +1,7 @@
-import { mergeConfig } from '@vendure/core';
-import { getBaseConfig } from '@shared/config';
+import { mergeConfig } from "@vendure/core";
+import { getBaseConfig } from "@shared/config";
 import "dotenv/config";
+import { CmsPlugin } from "./plugins/cms/cms.plugin";
 
 const baseConfig = getBaseConfig();
 
@@ -8,6 +9,10 @@ export const config = mergeConfig(baseConfig, {
   plugins: [
     ...(baseConfig.plugins ?? []),
     // Add your custom plugins here
+    CmsPlugin.init({
+      cmsApiKey: process.env.CONTENTFUL_API_KEY,
+      contentfulSpaceId: process.env.CONTENTFUL_SPACE_ID,
+    }),
   ],
   // Add any other overrides here
 });
